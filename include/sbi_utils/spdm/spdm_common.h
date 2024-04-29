f94e7f1d2d95958360ba2bfdc84845eb7f5ce653
offreitas <offreitas@usp.br>
2024-04-29 15:35:48 -0300

    SPDM session init function

--- include/sbi_utils/spdm/spdm_common.h
+++ include/sbi_utils/spdm/spdm_common.h
@@ -3,17 +3,21 @@
 
 #ifndef LIBSPDM_STDINT_ALT
 #define LIBSPDM_STDINT_ALT "sbi/sbi_types.h"
+#endif
 
 #include "hal/base.h"
 #include "hal/library/memlib.h"
+#include "industry_standard/spdm.h"
 #include "library/spdm_common_lib.h"
-#include "spdm_device_secret_lib_internal.h"
+#include "library/spdm_requester_lib.h"
+#include "library/spdm_transport_mctp_lib.h"
+#include "spdm_device_secret_lib_sample/spdm_device_secret_lib_internal.h"
 
 #define LIBSPDM_MAX_MSG_SIZE 0x1200
 #define LIBSPDM_TRANSPORT_HEADER_SIZE 64 
 #define LIBSPDM_TRANSPORT_TAIL_SIZE 64
 #define LIBSPDM_TRANSPORT_ADDITIONAL_SIZE \
-  (LIBSPDM_TRANSPORT_HEADER_SIZE + lIBSPDM_TRANSPORT_TAIL_SIZE)
+  (LIBSPDM_TRANSPORT_HEADER_SIZE + LIBSPDM_TRANSPORT_TAIL_SIZE)
 
 #define LIBSPDM_SENDER_BUFFER_SIZE (0x1000 + \
                                     LIBSPDM_TRANSPORT_ADDITIONAL_SIZE)
@@ -25,6 +29,26 @@
 #define SOCKET_TRANSPORT_TYPE_PCI_DOE 0x02
 #define SOCKET_TRANSPORT_TYPE_TCP 0x03
 
+#define EXE_CONNECTION_VERSION_ONLY 0x1 
+#define EXE_CONNECTION_DIGEST 0x2 
+#define EXE_CONNECTION_CERT 0x4 
+#define EXE_CONNECTION_CHAL 0x8 
+#define EXE_CONNECTION_MEAS 0x10 
+#define EXE_CONNECTION_SET_CERT 0x20 
+#define EXE_CONNECTION_GET_CSR 0x40
+
+#define EXE_SESSION_KEY_EX 0x1
+#define EXE_SESSION_PSK 0x2
+#define EXE_SESSION_NO_END 0x4
+#define EXE_SESSION_KEY_UPDATE 0x8
+#define EXE_SESSION_HEARTBEAT 0x10
+#define EXE_SESSION_MEAS 0x20
+#define EXE_SESSION_SET_CERT 0x40
+#define EXE_SESSION_GET_CSR 0x80
+#define EXE_SESSION_DIGEST 0x100
+#define EXE_SESSION_CERT 0x200
+#define EXE_SESSION_APP 0x400
+
 struct spdm_ops {
   // Message exchange functions
   libspdm_return_t (*send_message) (void *spdm_context,
@@ -32,7 +56,7 @@ struct spdm_ops {
                                     const void *request,
                                     uint64_t timeout);
   libspdm_return_t (*receive_message) (void *spdm_context,
-                                       size_t response_size,
+                                       size_t *response_size,
                                        void **response,
                                        uint64_t timeout);
 
@@ -43,7 +67,7 @@ struct spdm_ops {
   libspdm_return_t (*acquire_receiver_buffer) (void *context,
                                                void **msg_but_ptr);
   void (*release_receiver_buffer) (void *context, const void *msg_buf_ptr);
-}
+};
 
 struct spdm_state {
   void *spdm_context;
@@ -53,7 +77,14 @@ struct spdm_state {
   uint8_t use_version;
   uint8_t use_secured_message_version;
   uint8_t use_slot_id;
+  uint8_t use_slot_count;
+  uint16_t use_req_asym_algo;
+  uint32_t use_capability_flags;
+  uint32_t use_hash_algo;
+  uint32_t use_measurement_hash_algo;
+  uint32_t use_requester_capability_flags;
   uint32_t use_transport_layer;
+  uint32_t use_asym_algo;
 
   uint8_t support_measurement_spec;
   uint8_t support_other_params_support;
@@ -62,15 +93,14 @@ struct spdm_state {
   uint16_t support_aead_algo;
   uint16_t support_req_asym_algo;
   uint16_t support_key_schedule_algo;
-  uint32_t use_requester_capability_flags;
-  uint32_t use_capability_flags;
   uint32_t support_asym_algo;
   uint32_t support_hash_algo;
 
   uint32_t exe_connection;
+  uint32_t exe_session;
 
   struct spdm_ops *spdm_state_ops;
-}
+};
 
 void *spdm_session_init(struct spdm_state *spdm_state);
 
--- lib/utils/spdm/spdm_common.c
+++ lib/utils/spdm/spdm_common.c
@@ -4,10 +4,26 @@
 
 void *spdm_session_init(struct spdm_state *spdm_state)
 {
+  bool res;
+  void *data;
+  void *data1;
+  void *hash;
+  void *hash1;
+  uint8_t index;
   uint8_t data8;
   uint16_t data16;
   uint32_t data32;
+  uint32_t requester_capabilities_flag;
+  uint32_t responder_capabilities_flag;
+  const uint8_t *root_cert;
+  const uint8_t *root_cert1;
   size_t scratch_buffer_size;
+  size_t data_size;
+  size_t data1_size;
+  size_t hash_size;
+  size_t hash1_size;
+  size_t root_cert_size;
+  size_t root_cert1_size;
   spdm_version_number_t spdm_version;
   libspdm_return_t status;
   libspdm_data_parameter_t parameter;
@@ -54,7 +70,7 @@ void *spdm_session_init(struct spdm_state *spdm_state)
    * */
   libspdm_register_device_buffer_func(
     spdm_state->spdm_context,
-    LIBSDPM_SENDER_BUFFER_SIZE,
+    LIBSPDM_SENDER_BUFFER_SIZE,
     LIBSPDM_RECEIVER_BUFFER_SIZE,
     spdm_state->spdm_state_ops->acquire_sender_buffer,
     spdm_state->spdm_state_ops->release_sender_buffer,
@@ -78,10 +94,10 @@ void *spdm_session_init(struct spdm_state *spdm_state)
   /*
    * Set connection parameters
    * */
-  if (spdm_state->spdm_version != 0) {
+  if (spdm_state->use_version != 0) {
     libspdm_zero_mem(&parameter, sizeof(parameter));
     parameter.location = LIBSPDM_DATA_LOCATION_LOCAL;
-    spdm_version = spdm_state->spdm_version << SPDM_VERSION_NUMBER_SHIFT_BIT;
+    spdm_version = spdm_state->use_version << SPDM_VERSION_NUMBER_SHIFT_BIT;
     libspdm_set_data(spdm_state->spdm_context,
                      LIBSPDM_DATA_SPDM_VERSION,
                      &parameter,
@@ -89,11 +105,11 @@ void *spdm_session_init(struct spdm_state *spdm_state)
                      sizeof(spdm_version));
   }
 
-  if (m_use_secured_message_version != 0) {
+  if (spdm_state->use_secured_message_version != 0) {
     libspdm_zero_mem(&parameter, sizeof(parameter));
     parameter.location = LIBSPDM_DATA_LOCATION_LOCAL;
     spdm_version = spdm_state->use_secured_message_version << SPDM_VERSION_NUMBER_SHIFT_BIT;
-    libspdm_set_data(spdm_context,
+    libspdm_set_data(spdm_state->spdm_context,
                      LIBSPDM_DATA_SECURED_MESSAGE_VERSION,
                      &parameter,
                      &spdm_version,
@@ -101,7 +117,7 @@ void *spdm_session_init(struct spdm_state *spdm_state)
   }
 
   libspdm_zero_mem(&parameter, sizeof(parameter));
-  parameter.location - LIBSPDM_DATA_LOCATION_LOCAL;
+  parameter.location = LIBSPDM_DATA_LOCATION_LOCAL;
 
   data8 = 0;
   libspdm_set_data(spdm_state->spdm_context,
@@ -202,6 +218,266 @@ void *spdm_session_init(struct spdm_state *spdm_state)
     return NULL;
   }
 
+  if (spdm_state->use_version == 0) {
+    libspdm_zero_mem(&parameter, sizeof(parameter));
+    parameter.location = LIBSPDM_DATA_LOCATION_CONNECTION;
+    data_size = sizeof(spdm_version);
+    libspdm_get_data(spdm_state->spdm_context,
+                     LIBSPDM_DATA_SPDM_VERSION,
+                     &parameter,
+                     &spdm_version,
+                     &data_size);
+    spdm_state->use_version = spdm_version >>SPDM_VERSION_NUMBER_SHIFT_BIT;
+  }
 
+  /*
+   * Get SPDM context configuration
+   * */
+  libspdm_zero_mem(&parameter, sizeof(parameter));
+  parameter.location= LIBSPDM_DATA_LOCATION_LOCAL;
+  data_size = sizeof(data32);
+  libspdm_get_data(spdm_state->spdm_context,
+                   LIBSPDM_DATA_CAPABILITY_FLAGS,
+                   &parameter,
+                   &data32,
+                   &data_size);
+  requester_capabilities_flag = data32;
+
+  libspdm_zero_mem(&parameter, sizeof(parameter));
+  parameter.location = LIBSPDM_DATA_LOCATION_CONNECTION;
+  data_size = sizeof(data32);
+  libspdm_get_data(spdm_state->spdm_context,
+                   LIBSPDM_DATA_CAPABILITY_FLAGS,
+                   &parameter,
+                   &data32,
+                   &data_size);
+  responder_capabilities_flag = data32;
+
+  /*
+   * Change connection and session based on responder/requester capabilities 
+   * */
+  if ((SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_CERT_CAP & responder_capabilities_flag) == 0) {
+    spdm_state->exe_connection &= ~EXE_CONNECTION_DIGEST;
+    spdm_state->exe_connection &= ~EXE_CONNECTION_CERT;
+    spdm_state->exe_session &= ~EXE_CONNECTION_DIGEST;
+    spdm_state->exe_session &= ~EXE_CONNECTION_CERT;
+  }
+  if ((SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_CHAL_CAP & responder_capabilities_flag) == 0) {
+    spdm_state->exe_connection &= ~EXE_CONNECTION_CHAL;
+  }
+  if ((SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_MEAS_CAP & responder_capabilities_flag) == 0) {
+    spdm_state->exe_connection &= ~EXE_CONNECTION_MEAS;
+    spdm_state->exe_session &= ~EXE_SESSION_MEAS;
+  }
+
+  if (((SPDM_GET_CAPABILITIES_REQUEST_FLAGS_KEY_EX_CAP & requester_capabilities_flag) == 0) ||
+      ((SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_KEY_EX_CAP & responder_capabilities_flag) == 0)) {
+    spdm_state->exe_session &= ~EXE_SESSION_KEY_EX;
+  }
+  if (((SPDM_GET_CAPABILITIES_REQUEST_FLAGS_PSK_CAP & requester_capabilities_flag) == 0) ||
+      ((SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_PSK_CAP & responder_capabilities_flag) == 0)) {
+    spdm_state->exe_session &= ~EXE_SESSION_PSK;
+  }
+  if (((SPDM_GET_CAPABILITIES_REQUEST_FLAGS_KEY_UPD_CAP & requester_capabilities_flag) == 0) ||
+      ((SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_KEY_UPD_CAP & responder_capabilities_flag) == 0)) {
+    spdm_state->exe_session &= ~EXE_SESSION_KEY_UPDATE;
+  }
+  if (((SPDM_GET_CAPABILITIES_REQUEST_FLAGS_HBEAT_CAP & requester_capabilities_flag) == 0) ||
+      ((SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_HBEAT_CAP & responder_capabilities_flag) == 0)) {
+    spdm_state->exe_session &= ~EXE_SESSION_HEARTBEAT;
+  }
+
+  if ((SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_SET_CERT_CAP & responder_capabilities_flag) == 0) {
+    spdm_state->exe_connection &= ~EXE_CONNECTION_SET_CERT;
+    spdm_state->exe_session &= ~EXE_SESSION_SET_CERT;
+  }
+  if ((SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_CSR_CAP & responder_capabilities_flag) == 0) {
+    spdm_state->exe_connection &= ~EXE_CONNECTION_GET_CSR;
+    spdm_state->exe_session &= ~EXE_SESSION_GET_CSR;
+  }
+
+  data_size = sizeof(data32);
+  libspdm_get_data(spdm_state->spdm_context,
+                   LIBSPDM_DATA_CONNECTION_STATE,
+                   &parameter,
+                   &data32,
+                   &data_size);
+  LIBSPDM_ASSERT(data32 == LIBSPDM_CONNECTION_STATE_NEGOTIATED);
+  libspdm_get_data(spdm_state->spdm_context,
+                   LIBSPDM_DATA_MEASUREMENT_HASH_ALGO,
+                   &parameter,
+                   &data32,
+                   &data_size);
+  spdm_state->use_measurement_hash_algo = data32;
+  libspdm_get_data(spdm_state->spdm_context,
+                   LIBSPDM_DATA_BASE_ASYM_ALGO,
+                   &parameter,
+                   &data32,
+                   &data_size);
+  spdm_state->use_asym_algo = data32;
+  libspdm_get_data(spdm_state->spdm_context,
+                   LIBSPDM_DATA_BASE_HASH_ALGO,
+                   &parameter,
+                   &data32,
+                   &data_size);
+  spdm_state->use_hash_algo = data32;
+  data_size = sizeof(data16);
+  libspdm_get_data(spdm_state->spdm_context,
+                   LIBSPDM_DATA_REQ_BASE_ASYM_ALG,
+                   &parameter,
+                   &data16,
+                   &data_size);
+  spdm_state->use_req_asym_algo = data16;
+
+  if ((spdm_state->use_requester_capability_flags &
+       SPDM_GET_CAPABILITIES_REQUEST_FLAGS_PUB_KEY_ID_CAP) != 0) {
+    spdm_state->use_slot_id = 0xFF;
+  }
+  if (((spdm_state->exe_connection & EXE_CONNECTION_CERT) == 0) &&
+       (spdm_state->use_slot_id != 0xFF)) {
+    spdm_state->exe_connection &= ~EXE_CONNECTION_CHAL;
+    spdm_state->exe_connection &= ~EXE_CONNECTION_MEAS;
+    spdm_state->exe_session &= ~EXE_SESSION_KEY_EX;
+    spdm_state->exe_session &= ~EXE_SESSION_MEAS;
+  }
+  if (spdm_state->use_slot_id == 0xFF) {
+    res = libspdm_read_responder_public_key(spdm_state->use_asym_algo, &data, &data_size);
+    if (res) {
+      libspdm_zero_mem(&parameter, sizeof(parameter));
+      parameter.location = LIBSPDM_DATA_LOCATION_LOCAL;
+      libspdm_set_data(spdm_state->spdm_context,
+                       LIBSPDM_DATA_PEER_PUBLIC_KEY,
+                       &parameter,
+                       data,
+                       data_size);
+      /* Do not free it */
+    } else {
+      sbi_printf("[SPDM] Failed at read_responder_public_key\n");
+      sbi_free(spdm_state->spdm_context);
+      spdm_state->spdm_context = NULL;
+      return NULL;
+    }
+    res = libspdm_read_requester_public_key(spdm_state->use_req_asym_algo, &data, &data_size);
+    if (res) {
+      libspdm_zero_mem(&parameter, sizeof(parameter));
+      parameter.location = LIBSPDM_DATA_LOCATION_LOCAL;
+      libspdm_set_data(spdm_state->spdm_context,
+                       LIBSPDM_DATA_LOCAL_PUBLIC_KEY,
+                       &parameter,
+                       data,
+                       data_size);
+      /* Do not free it */
+    } else {
+      sbi_printf("[SPDM] Failed at read_requester_public_key\n");
+      sbi_free(spdm_state->spdm_context);
+      spdm_state->spdm_context = NULL;
+      return NULL;
+    }
+  } else {
+    res = libspdm_read_responder_root_public_certificate(spdm_state->use_hash_algo,
+                                                         spdm_state->use_asym_algo,
+                                                         &data, &data_size,
+                                                         &hash, &hash_size);
+    if (res) {
+      libspdm_x509_get_cert_from_cert_chain((uint8_t *)data + sizeof(spdm_cert_chain_t) + hash_size,
+                                            data_size - sizeof(spdm_cert_chain_t) - hash_size, 0,
+                                            &root_cert, &root_cert_size);
+      libspdm_zero_mem(&parameter, sizeof(parameter));
+      parameter.location = LIBSPDM_DATA_LOCATION_LOCAL;
+      libspdm_set_data(spdm_state->spdm_context,
+                      LIBSPDM_DATA_PEER_PUBLIC_ROOT_CERT,
+                      &parameter,
+                      (void *)root_cert,
+                      root_cert_size);
+      /* Do not free it */
+    } else {
+      sbi_printf("[SPDM] Failed at read_responder_root_public_certificate\n");
+      sbi_free(spdm_state->spdm_context);
+      spdm_state->spdm_context = NULL;
+      return NULL;
+    }
+
+    res = libspdm_read_responder_root_public_certificate_slot(1,
+                                                              spdm_state->use_hash_algo,
+                                                              spdm_state->use_asym_algo,
+                                                              &data1, &data1_size,
+                                                              &hash1, &hash1_size);
+    if (res) {
+      libspdm_x509_get_cert_from_cert_chain((uint8_t *)data1 + sizeof(spdm_cert_chain_t) + hash1_size,
+                                            data1_size - sizeof(spdm_cert_chain_t) - hash1_size, 0,
+                                            &root_cert1, &root_cert1_size);
+      libspdm_zero_mem(&parameter, sizeof(parameter));
+      parameter.location = LIBSPDM_DATA_LOCATION_LOCAL;
+      libspdm_set_data(spdm_state->spdm_context,
+                       LIBSPDM_DATA_PEER_PUBLIC_ROOT_CERT,
+                       &parameter,
+                       (void *)root_cert1,
+                       root_cert1_size);
+      /* Do not free it */
+    } else {
+      sbi_printf("[SPDM] Failed at read_responder_root_public_certificate_slot\n");
+      sbi_free(spdm_state->spdm_context);
+      spdm_state->spdm_context = NULL;
+      return NULL;
+    }
+  }
+
+  if (spdm_state->use_req_asym_algo != 0) {
+    res = libspdm_read_requester_public_certificate_chain(spdm_state->use_hash_algo,
+                                                          spdm_state->use_req_asym_algo,
+                                                          &data, &data_size, NULL, NULL);
+    if (res) {
+      libspdm_zero_mem(&parameter, sizeof(parameter));
+      parameter.location = LIBSPDM_DATA_LOCATION_LOCAL;
+      for (index = 0; index < spdm_state->use_slot_count; index++) {
+        parameter.additional_data[0] = index;
+        libspdm_set_data(spdm_state->spdm_context,
+                         LIBSPDM_DATA_LOCAL_PUBLIC_CERT_CHAIN,
+                         &parameter,
+                         data,
+                         data_size);
+        data8 = (uint8_t)(0xB0 + index);
+        libspdm_set_data(spdm_state->spdm_context,
+                         LIBSPDM_DATA_LOCAL_KEY_PAIR_ID,
+                         &parameter,
+                         &data8,
+                         sizeof(data8));
+        data8 = SPDM_CERTIFICATE_INFO_CERT_MODEL_DEVICE_CERT;
+        libspdm_set_data(spdm_state->spdm_context,
+                         LIBSPDM_DATA_LOCAL_CERT_INFO,
+                         &parameter,
+                         &data8, sizeof(data8));
+        data16 = SPDM_KEY_USAGE_BIT_MASK_KEY_EX_USE |
+                 SPDM_KEY_USAGE_BIT_MASK_CHALLENGE_USE |
+                 SPDM_KEY_USAGE_BIT_MASK_MEASUREMENT_USE |
+                 SPDM_KEY_USAGE_BIT_MASK_ENDPOINT_INFO_USE;
+        libspdm_set_data(spdm_state->spdm_context,
+                         LIBSPDM_DATA_LOCAL_KEY_USAGE_BIT_MASK,
+                         &parameter,
+                         &data16,
+                         sizeof(data16));
+        /* Do not free it */
+      }
+    } else {
+      sbi_printf("[SPDM] Failed at read_requester_public_certificate_chain\n");
+      sbi_free(spdm_state->spdm_context);
+      spdm_state->spdm_context = NULL;
+      return NULL;
+    }
+  }
+
+  libspdm_zero_mem(&parameter, sizeof(parameter));
+  parameter.location = LIBSPDM_DATA_LOCATION_LOCAL;
+  data8 = 0;
+  for (index = 0; index < spdm_state->use_slot_count; index++) {
+    data8 |= (1 << index);
+  }
+  libspdm_set_data(spdm_state->spdm_context,
+                   LIBSPDM_DATA_LOCAL_SUPPORTED_SLOT_MASK,
+                   &parameter,
+                   &data8,
+                   sizeof(data8));
+
+  return NULL;
 }
 
